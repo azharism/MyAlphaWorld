729d54caf5fdaa46f513ea01f0d107bf24ab2acb
azhar <azharism.007@gmail.com>
2023-01-15 03:26:46 +0530

    first commit

---
+++ MyAlphaWorld.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyAlphaWorld.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
